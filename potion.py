c3d4f365aae5e79d12099bc979e3bb43855812a0
phathway <22664626+pamelahathway@users.noreply.github.com>
2021-08-22 09:16:45 +0200

    Initial commit

---
+++ potion.py
@@ -0,0 +1,79 @@
+from potion_stuff import *
+
+
+class Potion:
+
+    def __init__(self, name):
+        """This is a class for brewing potions."""
+        self.colour = 'there-is-no-potion-so-the-potion-has-no-color'
+        self.cooked = False
+        self.container = None
+        self.heat_source = None
+        self.ingredients = []
+        self.simmer_duration = -1
+        self.name = name
+
+    def setup(self, container=None, heat_source=None):
+        """Add a container and/or heat_source to the potion.
+
+        Updates container and heat_source attributes in the class instance.
+
+        Parameters
+        ----------
+        container : str, optional
+            The name of the container to brew the potion in.
+        heat_source : str, optional
+            The name of the heat source used to cook the potions
+        """
+        if container == None:
+            print(f'You have not specified a container - where do you think you will brew your potion?')
+        if heat_source == None:
+            print(f'You have not specified a heat source - how will you cook the potion?')
+        self.container = container
+        self.heat_source = heat_source
+
+    def add_ingredients(self, ingredients=None):
+        """Add ingredients to the potion.
+
+        Updates ingredients and colour attributes in the class instance.
+
+        Parameters
+        ----------
+        ingredients : array_like, optional
+            A list of ingredients (str) to add to the potion.
+        """
+        if ingredients is None:
+            print(f'You have added no ingredients - have you spilt them on the floor again?')
+        else:
+            self.ingredients = ingredients
+            self.colour = "transparent"
+
+
+def make_example_potion(name):
+
+    my_potion = Potion(name=name)
+    # Set up your old kettle and light an eternal flame underneath it.
+    my_potion.setup(container=old_kettle, heat_source=eternal_flame)
+    # Simmer for 5 hours.
+    simmer(my_potion, duration=5)
+
+    return my_potion
+
+
+def importing_check():
+    print("you could access this function in potion.py, yey, well done!")
+
+
+my_name = 'ASPP participant'
+my_potion = make_example_potion(my_name)
+# Let Snape inspect the potion
+inspection_by_Snape(potion=my_potion, target_potion='example_potion')
+
+
+def make_python_expert_potion(name):
+    print("I am a Python Expert")
+    # todo: write this function!
+    pass
+
+
+
---
+++ potion_stuff.py
@@ -0,0 +1,157 @@
+import time
+
+
+# containers
+pewter_cauldron = 'pewter_cauldron'
+copper_cauldron = 'copper_cauldron'
+martini_glass = 'martini_glass'
+old_kettle = 'old_kettle'
+
+# ingredients
+snake_skin = 'snake_skin'
+fish_eyes = 'fish_eyes'
+
+
+def stir(potion, direction):
+    """Stirs the potion.
+
+    Updates colour in the class instance.
+
+    Parameters
+    ----------
+    potion : Potion instance
+        The potion to be stirred.
+    direction : {'clockwise', 'anti-clockwise'} str
+        The direction in which the potions is to be stirred
+    """
+    if direction == "clockwise":
+        potion.colour = "vomit-yellow"
+        print('NO!! Your potion turns vomit-yellow. Did you stir in the right direction?')
+        return
+    if direction == "anti-clockwise":
+        potion.colour = "newt-green"
+        print('Your potion turns a lovely newt-green.')
+        return
+
+
+def simmer(potion, duration):
+    """Cooks the potion.
+
+    Updates simmer_duration and cooked attributes in the class instance.
+
+    Parameters
+    ----------
+    potion : Potion instance
+        The potion to be cooked.
+    duration : int
+        How long to cook the potion for [hours].
+    """
+    potion.simmer_duration = duration
+    if duration < 2:
+        print('Are you sure you are cooking the potion enough? Your ingredients look a bit raw...')
+    elif duration > 5:
+        print('Oops, you have fallen asleep at your desk! Are you sure you want to simmer this long?')
+    else:
+        potion.cooked = True
+    return
+
+
+unicorn_hair = 'unicorn_hair'
+tea_leaves = 'tea_leaves'
+
+
+def inspection_by_Snape(potion, target_potion='python_expert'):
+    """Checks if potion was brewed correctly.
+
+    Prints narration of inspection process - read to see if potion passed inspection.
+    Snape checks container, heat_source, ingredients, and whether potion was cooked.
+    If something is wrong, function returns at that point.
+
+    Parameters
+    ----------
+    potion : obj 
+        Instance of Potion (class from potion_class).
+    target_potion: str, optional
+        Name of potion to be checked by Snape. Currently possible potions are 'python expert', 'example_potion'
+    """
+
+    print('-------------------------------')
+    print(f'A sour looking Snape walks towards you to inspect your {target_potion} potion.')
+    print(f'"What do we have here, {potion.name}...?"')
+    print_delay_dots()
+
+    if not potion:
+        print(f'"There is no potion I can inspect, {potion.name}, Please set up your potion!"')
+        print(f'    (Tip: are you actually passing a proper potion to Snape?)')
+        return
+
+    # set variables for each potion that need to be checked
+    if target_potion == 'python_expert':
+        expected_container = 'pewter_cauldron'
+        expected_heat_source = 'fire'
+        expected_ingredients = ['fish_eyes', 'tea_leaves', 'unicorn_hair']
+        expected_cooked = True
+        expected_simmer_duration = 2
+    elif target_potion == 'example_potion':
+        expected_container = 'old_kettle'
+        expected_heat_source = 'eternal_flame'
+        expected_ingredients = []
+        expected_cooked = True
+        expected_simmer_duration = 5
+    else:
+        print(f'"What is this, {potion.name}? This is not the name of an existing potion, check your spelling!"')
+        print(f'    (Target potion was not recognised, please check your spelling.)')
+        return
+
+    # check that correct setup was used
+    if potion.container == expected_container and potion.heat_source == expected_heat_source:
+        print(f'You have used the correct setup, Snape cannot complain - he looks even more sour.')
+    else:
+        print(f'Snape smirks and remarks "You have used the wrong cauldron or heat, {potion.name}!" \n'
+              f'With a flick of his wand he vanishes the potion. \n'
+              f'"I am taking 10 points from Ravenclaw, {potion.name}. Start again!"')
+        return
+
+    print_delay_dots()
+
+    # check if all ingredients are there
+    if sorted(potion.ingredients) == expected_ingredients:
+        print(f'You have used the correct ingredients, Snape cannot complain - his face darkens.')
+    else:
+        print(f'Snape smirks and remarks "You have used the wrong ingredients, {potion.name}!" \n'
+              f'With a flick of his wand he vanishes the potion. \n'
+              f'"I am taking 10 points from Gryffindor, {potion.name}. Start again!"')
+        return
+
+    print_delay_dots()
+
+    # check that potion is cooked
+    if potion.cooked == expected_cooked and potion.simmer_duration == expected_simmer_duration:
+        print(f'The potion is cooked properly, Snape cannot complain - he is looking annyoyed now.')
+    else:
+        print(f'Snape smirks and remarks "Your potion is not properly cooked!" \n'
+              f'With a flick of his wand he vanishes the potion. \n'
+              f'"I am taking 10 points from Hufflepuff, {potion.name}. Start again!"')
+        return
+
+    print_delay_dots()
+
+    print(f'Snape mutters "You got away this time, {potion.name}!", since there is nothing wrong with '
+          f'your {target_potion} potion. \n \n'
+          f'You pack your bags and leave as fast as you can to have a butterbeer at the lake!')
+
+    return
+
+
+# heat sources
+fire = 'fire'
+eternal_flame = 'eternal_flame'
+breathe_on_cauldron = 'breathe_on_cauldron'
+
+
+def print_delay_dots(dur=1, number=2):
+    for i in range(number):
+        time.sleep(dur)
+        print('.')
+
+
