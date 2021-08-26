a26c3edefc85c2dfb9e99870f312798c0c0f0664
ASPP Student <student@localhost>
2021-08-26 10:28:58 +0200

    complete exercise_importing

--- import_package.py
+++ import_package.py
@@ -10,10 +10,14 @@
 #print(tea_leaves)
 
 ##import all the objects in the brewing package
-import brewing.cooking
-import brewing.ingredients
-import brewing.containers
-import brewing.inspection
-import brewing.make_potion
-import brewing.potion
-print(brewing.containers.old_kettle)
+#import brewing.cooking
+#import brewing.ingredients
+#import brewing.containers
+#import brewing.inspection
+#import brewing.make_potion
+#import brewing.potion
+#print(brewing.containers.old_kettle)
+
+#write import cooking in the __init__.py file
+import brewing as brw
+print(brw.cooking.eternal_flame)
